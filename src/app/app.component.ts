f065f67c77f76745b18095f0ed3b5c373d828613
Austin Allman <ajallman@stkate.edu>
2018-07-24 14:31:38 -0500

    Added angular support for the github pages SPA fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,20 @@ export class AppComponent implements OnInit{
   constructor(private blogService:BlogService,
               private router:Router){}
   ngOnInit(): void {
+    this.fixGithubSPA();
     this.categories$ = this.blogService.getCategories();
+  }
+  fixGithubSPA(){
+    /* 
+    The script from https://github.com/rafrex/spa-github-pages
+    puts a extra slash in the pathname 
+    //example
+    check for that and navigate to it if it's present
+    */
+    const loc:string = decodeURIComponent(window.location.pathname);
+    if(loc.substring(0,2) === "//"){
+      this.router.navigate([loc]);
+    }
   }
   showCatogryArticles(category:string){
     this.router.navigate(['/article',category]);
